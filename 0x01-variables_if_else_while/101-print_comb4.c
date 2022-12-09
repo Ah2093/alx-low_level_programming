84c0c51f74c1c50b8a99d6202d98c2cb157ce755
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-09 09:18:47 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,28 +8,28 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		int j;
-		
-		for (j = i+1; j < 9; j++)
+
+		for (j = i + 1; j < 9; j++)
 		{
 			int l;
-			
-		       	for (l = j + 1 ; l < 10; l++)
+
+			for (l = j + 1 ; l < 10; l++)
 			{
 				putchar(i + '0');
-			 	putchar(j + '0');
-			 	putchar(l + '0');
-			 	if (i != 7)
-			 	{
+				putchar(j + '0');
+				putchar(l + '0');
+				if (i != 7)
+				{
 					putchar(',');
 					putchar(' ');
-			       	}
-			 }
-		 }
+				}
+			}
+		}
 	}
-	putchar('\n');	
+	putchar('\n');
 	return (0);
 }
