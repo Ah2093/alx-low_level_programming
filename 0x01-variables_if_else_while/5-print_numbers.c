984696abb3b9fc6d665943102d39e2ead7540f55
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-09 08:17:18 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for (int letter =0; letter <= 9; letter++)
+		printf(letter);
+	printf('\n');
+	return (0);
+}
