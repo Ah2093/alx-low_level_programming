aa33881017995bb480129825eadf39bea8e387c7
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-09 08:34:53 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 	char letter;
-	int i ;
-	for (i=0 ; i<=9 ; i++)
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
 		putchar( i + '0');
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
