4da76aac2dd6aff0488585c9d403c6a112410e8a
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-09 08:33:08 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+	int i ;
+	for (i=0 ; i<=9 ; i++)
+		putchar( i + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
