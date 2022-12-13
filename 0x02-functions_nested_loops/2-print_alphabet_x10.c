dd841e555fd1cecc45e8f8e5ce1557d7a9371a2a
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-13 06:53:20 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+
+void print_alphabet_x10(void)
+{
+	int letter;
+
+	for (letter = 0; letter <= 9; letter++)
+		print_alphabet();
+}
