5c59e2ebde57ce65ba62cd90aab1ce143a143627
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-13 08:24:14 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - a function print n1 >> 98
+ * @n1: the first intger as input
+ * print : the number between n1 and 98
+ */
+
+void print_to_98(int n1)
+{
+	int n;
+
+	if (n1 < 98)
+	{
+		for (n = n1 ; n <= 98 ; n++)
+			printf("%d, ", n);
+	}
+	else
+	{
+		for (n = n1 ; n >= 98 ; n--)
+			printf("%d, ", n);
+	}
+}
