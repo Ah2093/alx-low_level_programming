4cf746f7281fc5d03bdd39a390822957b6a21501
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-15 09:51:03 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * more_numbers - a function that prints 10 times the numbers,
- * from 0 to 14, followed by a new line.
  * Return: void
  */
 void more_numbers(void)
@@ -15,5 +14,4 @@ void more_numbers(void)
 			_putchar(j + '0');
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
