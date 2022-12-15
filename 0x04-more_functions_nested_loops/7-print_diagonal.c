8730b89d688c9f9b62f7d5d85c3b7f5ad31c373d
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-15 10:28:36 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a straight line in the terminal
+ * @n: input number of times to print '_'
+ * Return: a straight line
+ */
+void print_diagonal(int n)
+{
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_puchar('\\');
+			_putchar('\n');
+		}
+	}
+}
