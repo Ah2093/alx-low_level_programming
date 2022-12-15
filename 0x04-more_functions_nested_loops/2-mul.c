66451f9fb1b34410efe6fd66a4ee9de37cd3acff
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-15 09:37:18 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - a function to check if the c is uppercase or not
+ * @a :paramter
+ * @b :paramter
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
