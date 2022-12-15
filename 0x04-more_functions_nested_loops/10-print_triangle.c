47157367756fb2b7e5ca5ee43ddce161b13fdbff
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-15 10:38:16 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - a function that draws a straight line in the terminal
+ * @n: input number of times to print '_'
+ * Return: a straight line
+ */
+void print_triangle(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+		
+		for (i = 0; i < n; i++)
+		{
+			for (j = n - i - 1 ; j > 0; j--)
+				_putchar(' ');
+			for (j =i ; j >= 0; j--)
+                                _putchar('#');
+			_putchar('\n');
+		}
+	}
+}
