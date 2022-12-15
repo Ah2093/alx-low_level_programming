0853c45738343b60137ee84c38c26ec363864ddb
Ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-15 10:01:15 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
 /**
- * print_line - a function that draws a straight line in the terminal.
- * @n :paramter
- * Return: void
+ * print_line - a function that draws a straight line in the terminal
+ * @n: input number of times to print '_'
+ * Return: a straight line
  */
-void print_numbers(int n)
+void print_line(int n)
 {
-	int i;
+	int co;
 
-	if (n >= 0)
-		for (i = 0; i < n ; i++)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
 			_putchar('_');
-	_putchar('\n');
+		}
+		_putchar('\n');
+	}
 }
