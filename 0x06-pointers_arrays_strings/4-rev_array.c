f74a579803ac4f44dd8cd60e85aab4687b952913
ahmed hamdi <ah2093@fayoum.edu.eg>
2022-12-22 09:21:59 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,13 +10,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, s;
+	int i, j, s;
 
-	n /= 2;
-	for (i = 0; i < n; i++)
+	j = n / 2;
+	for (i = 0; i < j; i++)
 	{
 		s = a[i];
-		a[i] = a[i + n];
-		a[i + n] = s;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = s;
 	}
 }
