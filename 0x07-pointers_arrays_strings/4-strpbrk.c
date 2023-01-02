4111dc828347362ebb4848249d5b38183ad101f6
ahmed hamdi <ah2093@fayoum.edu.eg>
2023-01-02 12:44:39 -0800

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *c)
+{
+	int i, j;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j =0 ;c[j] >='\0';j++)
+			if (s[i] == c[j])
+				return (s + i);
+	}
+
+	return (NULL);
+}
