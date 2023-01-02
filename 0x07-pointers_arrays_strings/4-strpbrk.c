347259048847914c503fd1eac42ec32e86f951b1
ahmed hamdi <ah2093@fayoum.edu.eg>
2023-01-02 12:45:58 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,7 +13,7 @@ char *_strpbrk(char *s, char *c)
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		for (j =0 ;c[j] >='\0';j++)
+		for (j =0 ; c[j] >= '\0'; j++)
 			if (s[i] == c[j])
 				return (s + i);
 	}
